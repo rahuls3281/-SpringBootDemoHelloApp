6b3aaa8e9120308dd7274dfc4811e59503177e9d
rahuls3281 <rahuls3281@gmail.com>
2025-10-26 16:07:16 +0530

    data

--- src/main/java/com/example/helloapp/service/EmployeeService.java
+++ src/main/java/com/example/helloapp/service/EmployeeService.java
@@ -1,8 +1,6 @@
 package com.example.helloapp.service;
 
-import com.example.helloapp.entity.Employee;
 import com.example.helloapp.repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
